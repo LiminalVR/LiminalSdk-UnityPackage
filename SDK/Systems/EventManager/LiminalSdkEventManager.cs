55f9d987ce2a9b4409dbda577b81c820fa7a0e45
Rob <rromanomusic@gmail.com>
2025-02-21 13:51:28 +1100

    Modify liminal sdk event manager to use a custom data class for events
    replaced action queue for a list of custom class 'ActionEventData' which houses the action as well as additional properties. Added additional methods for calling individual actions as well as multiple actions at once. Converted methods into IEnumerators to allow for delayed actions and to make it more scalable in future.

--- SDK/Systems/EventManager/LiminalSdkEventManager.cs
+++ SDK/Systems/EventManager/LiminalSdkEventManager.cs
@@ -1,56 +1,124 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+
 
 namespace Liminal.Shared
 {
     public static class LiminalSdkEventManager
     {
 
-        private static Queue<Action> _queuedPlatformActions = new Queue<Action>();
-        private static Queue<Action> _queuedExperienceActions = new Queue<Action>();
+        private static List<ActionEventData> _platformActionDataList = new List<ActionEventData>();
+        private static List<ActionEventData> _experienceActionDataList = new List<ActionEventData>();
 
         /// <summary>
-        /// Queues an event from the experience / liminal SDK side that can be triggered by the platform when necessary
+        /// Add actionEvent to list from the experience / liminal SDK side that can be triggered by the platform when necessary
+        /// </summary>
+        /// <param name="eventData"></param>
+        public static void QueueEventForPlatform(ActionEventData eventData)
+        {
+            _platformActionDataList.Add(eventData);
+        }
+
+        /// <summary>
+        /// Add actionEvent to list from platform side that can be triggered by an experience when necessary
         /// </summary>
         /// <param name="evnt"></param>
-        public static void QueueEventForPlatform(Action evnt)
+        public static void QueueEventForExperience(ActionEventData eventData)
         {
-            if (!_queuedPlatformActions.Contains(evnt))
+            _experienceActionDataList.Add(eventData);
+        }
+
+        /// <summary>
+        /// Triggers the first actionEvent in the platformActionEvent list
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerator TriggerFirstPlatformActionEvent()
+        {
+            yield return TriggerFirstActionEvent(_platformActionDataList);
+        }
+
+        /// <summary>
+        /// Triggers the first actionEvent in the experienceActionEvent list
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerator TriggerFirstExperienceActionEvent()
+        {
+            yield return TriggerFirstActionEvent(_experienceActionDataList);
+        }
+
+
+        private static IEnumerator TriggerFirstActionEvent(List<ActionEventData> list)
+        {
+            var eventData = list.FirstOrDefault();
+            yield return TriggerEvent(eventData);
+            list.Remove(eventData);
+        }
+
+        /// <summary>
+        /// Triggers all actionEvents within the platformActionEventList with matching Id
+        /// </summary>
+        /// <param name="id">Id of the actionEvent</param>
+        /// <returns></returns>
+        public static IEnumerator TriggerAllPlatformActionEventsWithId(string id)
+        {
+            yield return TriggerAllActionEventsWithId(id, _platformActionDataList);
+        }
+
+        /// <summary>
+        /// Triggers all actionEvents within the experienceActionEventList with matching Id
+        /// </summary>
+        /// <param name="id">Id of the actionEvent</param>
+        /// <returns></returns>
+        public static IEnumerator TriggerAllExperienceActionEventsWithId(string id)
+        {
+            yield return TriggerAllActionEventsWithId(id, _experienceActionDataList);
+        }
+
+        private static IEnumerator TriggerAllActionEventsWithId(string id, List<ActionEventData> list)
+        {
+            // Get all actionEvents with matching id, trigger each event and then remove from list
+            var matchingEvents = list.FindAll(x => x.Id.Equals(id));
+
+            foreach (var eventData in matchingEvents)
             {
-                _queuedPlatformActions.Enqueue(evnt);
+                yield return TriggerEvent(eventData);
             }
+
+            matchingEvents.ForEach(eventData => list.Remove(eventData));
         }
 
-        /// <summary>
-        /// Queues an event from the platform side that can be triggered by an experience when necessary
-        /// </summary>
-        /// <param name="evnt"></param>
-        public static void QueueEventForExperience(Action evnt)
+        private static IEnumerator TriggerEvent(ActionEventData eventData)
         {
-            if (!_queuedExperienceActions.Contains(evnt))
+            // Get all actionEvents with matching id, trigger each event and then remove from list
+            yield return new WaitForSeconds(eventData.Delay);
+            eventData.Action?.Invoke();
+        }
+
+
+
+        public class ActionEventData
+        {
+            public string Id;
+            public float Delay;
+            public Action Action;
+
+            public ActionEventData(Action action)
+                : this(Guid.NewGuid().ToString(), action) { }
+
+            public ActionEventData(string id, Action action, float delay = 0)
             {
-                _queuedExperienceActions.Enqueue(evnt);
+                Id = id;
+                Action = action;
+                Delay = delay;
             }
-        }
 
-        /// <summary>
-        /// Dequeue and trigger the next event in the platformEvent queue.
-        /// </summary>
-        public static void TriggerPlatformEvent()
-        {
-            var action = _queuedPlatformActions.Dequeue();
-            action?.Invoke();
-        }
-
-        /// <summary>
-        /// Dequeue and trigger the next event in the experienceEvent queue.
-        /// </summary>
-        public static void TriggerExperienceEvent()
-        {
-            var action = _queuedExperienceActions.Dequeue();
-            action?.Invoke();
+            public void Trigger()
+            {
+                Action?.Invoke();
+            }
         }
     }
 
