0e690d1e058dda7219329ca24b38d62467b0da09
Tinny <tinnystudios@gmail.com>
2026-05-19 11:42:26 +1000

    Fix up the scene hooks

--- SDK/Editor/Builder/Windows/ExperienceAppPlayModeHook.cs
+++ SDK/Editor/Builder/Windows/ExperienceAppPlayModeHook.cs
@@ -1,13 +1,12 @@
-using Liminal.SDK;
+using Liminal.SDK.V2;
 using UnityEditor;
+using UnityEngine;
 
 namespace Liminal.SDK.Build
 {
     [InitializeOnLoad]
     internal static class ExperienceAppPlayModeHook
     {
-        private const string ExperienceAppObjectName = "[ExperienceApp]";
-
         static ExperienceAppPlayModeHook()
         {
             EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
@@ -15,12 +14,35 @@ namespace Liminal.SDK.Build
 
         private static void OnPlayModeStateChanged(PlayModeStateChange state)
         {
-            if (state != PlayModeStateChange.ExitingEditMode)
-                return;
+            var app = FindAssignedExperienceApp();
 
-            var app = GameObjectUtils.FindInactiveByName(ExperienceAppObjectName);
-            if (app != null && app.activeSelf)
-                app.SetActive(false);
+            switch (state)
+            {
+                case PlayModeStateChange.ExitingEditMode:
+                    Debug.Log("Entering Play Mode");
+                    if (app != null && app.activeSelf)
+                        app.SetActive(false);
+
+                    break;
+                case PlayModeStateChange.EnteredEditMode:
+                    Debug.Log("Entering Edit Mode");    
+                    if (app != null && !app.activeSelf)
+                        app.SetActive(true);
+                    break;
+            }
+
+        }
+
+        private static GameObject FindAssignedExperienceApp()
+        {
+            var managers = Object.FindObjectsByType<ExperienceAppManager>(FindObjectsInactive.Include);
+            foreach (var manager in managers)
+            {
+                if (manager != null && manager.ExperienceApp != null)
+                    return manager.ExperienceApp.gameObject;
+            }
+
+            return null;
         }
     }
 }
